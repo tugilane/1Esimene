7cc6904ed3245f2c51306925fd70859fdebf2684
tugilane <marten.peljo@gmail.com>
2023-04-22 11:38:45 +0300

    data rendering, date conversion ei tööta!

--- projekt-ilmateade/main.js
+++ projekt-ilmateade/main.js
@@ -1,6 +1,52 @@
 import { getWeather } from "./weather";
+import { ICON_MAP } from "./iconMap";
 
-getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(data => {
-    console.log(data)
-} )
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(renderWeather).catch(e => {
+    console.error(e)
+    alert("Error getting weather.")
+})
 
+
+
+function renderWeather({ current, daily, hourly}) {
+   /* renderCurrentWeather(current)
+    renderDailyWeather(daily)
+    renderHourlyWeather(hourly)  */
+    document.body.classList.remove("blurred")
+}
+
+function setValue(selector, value, {parent = document} = {}) {
+    parent.querySelector(`[data-${selector}]`).textContent = value
+}
+
+function getIconUrl(iconCode) {
+    return `icons/${ICON_MAP.get(iconCode)}.svg`
+}
+
+const currentIcon = document.querySelector("[data-current-icon]")
+function renderCurrentWeather(current) {
+    currentIcon.src = getIconUrl(current.iconCode)
+setValue("current-temp", current.currentTemp)
+setValue("current-high", current.highTemp)
+setValue("current-low", current.lowTemp)
+setValue("current-sunrise", current.sunrise)
+setValue("current-sunset", current.sunset)
+setValue("current-wind", current.windSpeed)
+setValue("current-precip", current.precip)
+
+}
+
+const DAY_FORMATTER = new Intl.dateTimeFormat(undefined, {weekday:
+    "long"})
+const dailySection = document.querySelector("data-day-section")
+const dayCardTemplate = document.getElementById("day-card-template")
+function renderDailyWeather(daily) {
+    dailySection.HTML = ""
+    daily.forEach(day => {
+        const element = dayCardTemplate.content.cloneNode(true)
+        setValue("temp;", day.maxTemp, {parent: element})
+        setValue("date;", DAY_FORMATTER.format(day.timestamp), {parent: element})
+        element.querySelector("[data-icon]").src = getIconUrl(day.iconCode)
+        dailySection.append(element)
+    })
+}
